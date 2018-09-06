de73d3939aff1fe6a69e862d3da4bf2a4c074a3c
zayymr <36123730+zayymr@users.noreply.github.com>
2018-09-05 20:21:09 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+<link rel="stylesheet" type="text/css"  href="css/flor.css">
+<title> Cuestionario V 1.1 </title>
+</head>
+<body>
+<h2>Cuestionario de Evaluación</h2>
+<p>En cada petalo se encuentra una pregunta por responder, por favor lee y responde con la mayor sinceridad posible. </p>
+<div class="drops">
+	<a href="/pregunta1" class="p1"></a>
+	<a href="/pregunta2" class="p2"></a>
+	<a href="/pregunta3" class="p3"></a>
+	<a href="/pregunta4" class="p4"></a>
+	<a href="/pregunta5" class="p5"></a>
+	<a href="/pregunta6" class="p6"></a>
+	<a href="/pregunta7" class="p7"></a>
+	<a href="/pregunta8" class="p8"></a>
+	<a href="/pregunta9" class="p9"></a>
+	<a href="/pregunta10" class="p10"></a>
+	<a href="/pregunta11" class="p11"></a>
+	<a href="/pregunta12" class="p12"></a>
+	<a href="/pregunta13" class="p13"></a>
+	<a href="/pregunta14" class="p14"></a>
+	<a href="/pregunta15" class="p15"></a>
+	<a href="/pregunta16" class="p16"></a>
+	<a href="/continuar" class="flowerCenter"></a>
+</div>
+<div class="button"</div>
+</body>
+</html>
+
